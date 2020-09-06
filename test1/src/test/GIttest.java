883bb7a9b7c5b67e0bc6626f5f7b8709156cce62
zss-zjs <1271850001@qq.com>
2020-09-06 12:48:30 +0800

    再次输出

--- test1/src/test/GIttest.java
+++ test1/src/test/GIttest.java
@@ -3,8 +3,7 @@ package test;
 public class GIttest {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println(
-                "hhhh"
-        );
+        System.out.println("hhhh");
+        System.out.println("nhidfaye");
     }
 }
